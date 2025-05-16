4c8b2ff10bdc954c19ffffa4f5b24377fe51e72f
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-16 13:36:08 +0900

    fix: DB에 날짜 저장 시 UTC로 저장

--- src/controllers/addCreatedAt.js
+++ src/controllers/addCreatedAt.js
@@ -2,20 +2,13 @@ async function addCreatedAtField(model, date = new Date()) {
   try {
     const result = await model.updateMany(
       { createdAt: { $exists: false } },
-      { $set: { createdAt: date } }
+      { $set: { createdAt: date.toISOString() } }
     );
 
     console.log(`Updated ${result.nModified} comments with createdAt field.`);
   } catch (err) {
     console.error('Error updating comments:', err);
   }
-}
-
-function convertKoreanDateToUTC(koreanDate) {
-  const [year, month, day] = koreanDate.split('.').map(Number);
-  const utcDate = new Date(Date.UTC(year, month - 1, day));
-
-  return utcDate;
 }
 
 module.exports = { addCreatedAtField, convertKoreanDateToUTC };
