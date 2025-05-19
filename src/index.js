2661868c00418db905c9ced055555169f3383a49
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-19 09:29:58 +0900

    feat: 배포 주소 cors 허용

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://pickmeup-plum.vercel.app/'],
     credentials: true,
   })
 );
